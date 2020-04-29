d7a283d27ba8c58dbb41dde4f468839d222ce2a0
Ravi Kallan <rak@contentguru.com>
2020-04-30 01:56:41 +0200

    on logout redirect to login page

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import useAuth from "./hooks/useAuth";
 import { logout } from ".";
 
 const Logout = () => {
     const auth = useAuth();
+    const history = useHistory();
 
     const onClickHandler = () => {
         logout();
+        history.push("/login");
     };
 
     if (!auth) return null;
