bea2f664878afc9871bf9d435e4452c4aeca1f07
Ravi Kallan <ravikallan@MacBook-Pro-van-Ravi.local>
2020-05-17 09:43:41 +0200

    removed unused var

--- src/rkallan-ui-library/InputTypeText/index.jsx
+++ src/rkallan-ui-library/InputTypeText/index.jsx
@@ -153,7 +153,6 @@ const InputTypeText = ({ label, attributes, variant, clearValue, defaultValue })
             )}
 
             <div className={styles.inputContainer}>
-                {inputState}
                 <animated.input
                     ref={inputElementRef}
                     id={`${label.for}-${randomAlphanumericInsensitive}`}
